a6585569c13c9fa9587a6fe87147dda0b841de67
Alex Frost <VoinovAlex1900@yandex.ru>
2025-12-15 21:04:08 +0300

    адаптивный футер

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,10 @@ import styles from './Footer.module.css'
 const Footer = () => {
     return (
         <footer className={styles.wrapper}>
-            <div>Copyright © 2025 Shift All rights reserved.</div>
-            <div>Privacy & Policy</div>
+            <div className={styles.container}>
+                <div>Copyright © 2025 Shift <span className={styles.additionalText}>&nbsp;All rights reserved.</span></div>
+                <div>Privacy & Policy</div>
+            </div>
         </footer>
     )
 }
